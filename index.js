d500ccc6b6f48e6057d03824137fdf8c73010ce3
Diamond-FoxUA <dimofarb@gmail.com>
2025-10-28 21:56:53 +0000

    Deploying to gh-pages from @ Diamond-FoxUA/simple-chocolate@dba6838cfc75e952ef16230a0239d40b202fa918 🚀

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import{S as v,N as g,P as b,K as y,A as w}from"./assets/vendor-CXw4zOXi.js";(function(){const l=document.createElement("link").relList;if(l&&l.supports&&l.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))f(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const n of t.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&f(n)}).observe(document,{childList:!0,subtree:!0});function h(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function f(e){if(e.ep)return;e.ep=!0;const t=h(e);fetch(e.href,t)}})();new v(".swiper",{modules:[g,b,y,w],loop:!0,autoplay:{delay:1e4,pauseOnMouseEnter:!1,disableOnInteraction:!1},slidesPerView:1,spaceBetween:10,breakpoints:{768:{slidesPerView:2,spaceBetween:16},1200:{slidesPerView:3,spaceBetween:28,loop:!1}},pagination:{el:".swiper-pagination",clickable:!0},keyboard:{enabled:!0}});const L=document.querySelector("[data-menu-open]"),E=document.querySelector("[data-menu-close]"),d=document.querySelector("[data-menu]"),p=document.querySelector("body");L.addEventListener("click",m);E.addEventListener("click",m);d.addEventListener("click",s=>{s.target.classList.contains("mobile-menu-nav-link")&&m()});function m(){if(d.classList.toggle("hidden"),d.classList.contains("hidden")){p.classList.remove("modal-open");return}p.classList.add("modal-open")}const k=document.querySelector("[data-modal-open]"),o=document.querySelector("[data-modal]"),q=document.body,x=`
+import{S as k,N as E,P as S,K as q,A as x,i as g}from"./assets/vendor-BbOGx51x.js";(function(){const l=document.createElement("link").relList;if(l&&l.supports&&l.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))p(t);new MutationObserver(t=>{for(const s of t)if(s.type==="childList")for(const r of s.addedNodes)r.tagName==="LINK"&&r.rel==="modulepreload"&&p(r)}).observe(document,{childList:!0,subtree:!0});function L(t){const s={};return t.integrity&&(s.integrity=t.integrity),t.referrerPolicy&&(s.referrerPolicy=t.referrerPolicy),t.crossOrigin==="use-credentials"?s.credentials="include":t.crossOrigin==="anonymous"?s.credentials="omit":s.credentials="same-origin",s}function p(t){if(t.ep)return;t.ep=!0;const s=L(t);fetch(t.href,s)}})();new k(".swiper",{modules:[E,S,q,x],loop:!0,autoplay:{delay:1e4,pauseOnMouseEnter:!1,disableOnInteraction:!1},slidesPerView:1,spaceBetween:10,breakpoints:{768:{slidesPerView:2,spaceBetween:16},1200:{slidesPerView:3,spaceBetween:28,loop:!1}},pagination:{el:".swiper-pagination",clickable:!0},keyboard:{enabled:!0}});const P=document.querySelector("[data-modal-open]"),o=document.querySelector("[data-modal]"),B=document.body,i=new URL("/simple-chocolate/assets/sprite-6iG90Qtl.svg",import.meta.url).href,O=`
   <div class="modal-container">
     <button class="modal-close-btn" data-modal-close></button>
     <h2 class="modal-heading">
@@ -11,7 +11,7 @@ import{S as v,N as g,P as b,K as y,A as w}from"./assets/vendor-CXw4zOXi.js";(fun
         <div class="form-input-container">
           <input class="modal-form-input" type="text" id="user-name" required />
           <svg class="form-input-icon" width="17" height="17">
-            <use href="/img/sprite.svg#icon-user"></use>
+            <use href="${i}#icon-user"></use>
           </svg>
         </div>
       </div>
@@ -21,7 +21,7 @@ import{S as v,N as g,P as b,K as y,A as w}from"./assets/vendor-CXw4zOXi.js";(fun
         <div class="form-input-container">
           <input class="modal-form-input" type="email" id="user-email" required />
           <svg class="form-input-icon" width="17" height="17">
-            <use href="/img/sprite.svg#icon-mail"></use>
+            <use href="${i}#icon-mail"></use>
           </svg>
         </div>
       </div>
@@ -31,7 +31,7 @@ import{S as v,N as g,P as b,K as y,A as w}from"./assets/vendor-CXw4zOXi.js";(fun
         <div class="form-input-container">
           <input class="modal-form-input" type="tel" id="user-phone" required />
           <svg class="form-input-icon" width="17" height="17">
-            <use href="/img/sprite.svg#icon-phone"></use>
+            <use href="${i}#icon-phone"></use>
           </svg>
         </div>
       </div>
@@ -51,7 +51,7 @@ import{S as v,N as g,P as b,K as y,A as w}from"./assets/vendor-CXw4zOXi.js";(fun
         <label class="modal-form-label-privacy" for="user-privacy">
           <span class="checkbox">
             <svg class="checkbox-icon" width="10" height="10">
-              <use href="/img/sprite.svg#icon-check"></use>
+              <use href="${i}#icon-check"></use>
             </svg>
           </span>
           <span class="checkbox-text">
@@ -64,5 +64,5 @@ import{S as v,N as g,P as b,K as y,A as w}from"./assets/vendor-CXw4zOXi.js";(fun
       <button class="modal-form-btn" type="submit">Send</button>
     </form>
   </div>
-`;o.insertAdjacentHTML("beforeend",x);const S=o.querySelector("[data-modal-close]");k.addEventListener("click",i);S.addEventListener("click",i);o.addEventListener("click",P);window.addEventListener("keydown",B);function i(){o.classList.toggle("hidden"),q.classList.toggle("modal-open")}function P(s){s.target===o&&i()}function B(s){s.key==="Escape"&&!o.classList.contains("hidden")&&i()}const r=document.querySelector(".header");let c=window.scrollY,u=!1,a=!1;function O(){const s=window.scrollY;s<50?(r.classList.remove("hidden"),a=!1):s>c&&!a?(r.classList.add("hidden"),a=!0):s<c&&a&&(r.classList.remove("hidden"),a=!1),c=s,u=!1}window.addEventListener("scroll",()=>{u||(window.requestAnimationFrame(O),u=!0)});
+`;o.insertAdjacentHTML("beforeend",O);const M=o.querySelector("[data-modal-close]");P.addEventListener("click",n);M.addEventListener("click",n);o.addEventListener("click",N);window.addEventListener("keydown",A);function n(){o.classList.toggle("hidden"),B.classList.toggle("modal-open")}function N(e){e.target===o&&n()}function A(e){e.key==="Escape"&&!o.classList.contains("hidden")&&n()}const Y=document.querySelector("[data-menu-open]"),$=document.querySelector("[data-menu-close]"),u=document.querySelector("[data-menu]"),h=document.querySelector("body");Y.addEventListener("click",f);$.addEventListener("click",f);u.addEventListener("click",e=>{e.target.classList.contains("mobile-menu-nav-link")&&f()});function f(){if(u.classList.toggle("hidden"),u.classList.contains("hidden")){h.classList.remove("modal-open");return}h.classList.add("modal-open")}const c=document.querySelector(".header");let d=window.scrollY,m=!1,a=!1;function I(){const e=window.scrollY;e<50?(c.classList.remove("hidden"),a=!1):e>d&&!a?(c.classList.add("hidden"),a=!0):e<d&&a&&(c.classList.remove("hidden"),a=!1),d=e,m=!1}window.addEventListener("scroll",()=>{m||(window.requestAnimationFrame(I),m=!0)});const b=document.querySelector(".btn-buy"),v=document.querySelector(".form-btn"),y=document.querySelector(".modal-form-btn");b&&b.addEventListener("click",()=>V("Sorry, not available at the moment."));v&&v.addEventListener("click",e=>{e.preventDefault(),w("You successfully subscribed!")});y&&y.addEventListener("click",e=>{e.preventDefault(),w("Thank you for your feedback!")});function w(e){g.success({position:"bottomRight",message:e})}function V(e){g.info({position:"bottomRight",message:e})}
 //# sourceMappingURL=index.js.map
